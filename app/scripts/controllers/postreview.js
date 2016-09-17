96d0ec4acb2bafa293259bb0d389c1c4b1ecd6da
Sergey Shevchenko <sergey.aegis@gmail.com>
2016-09-18 02:03:32 +0700

    blank controllers and templates

---
+++ app/scripts/controllers/postreview.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name restRevApp.controller:PostreviewCtrl
+ * @description
+ * # PostreviewCtrl
+ * Controller of the restRevApp
+ */
+angular.module('restRevApp')
+  .controller('PostreviewCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/restaurantlist.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name restRevApp.controller:RestaurantlistCtrl
+ * @description
+ * # RestaurantlistCtrl
+ * Controller of the restRevApp
+ */
+angular.module('restRevApp')
+  .controller('RestaurantlistCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
