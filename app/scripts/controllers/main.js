7be6ec912d119641e57ca0758eb6da8385292471
Sergey Shevchenko <sergey.aegis@gmail.com>
2016-09-21 13:18:29 +0700

    feat: filter restaurants by view

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -9,14 +9,50 @@ angular.module('restRevApp')
 .controller('MainCtrl', [
     '$scope',
     'RestData',
-function ($scope, RestData) {
+    '$mdToast',
+function ($scope, RestData, $mdToast) {
     'use strict';
     var main = this;
+    var originatorEv;
+    var allData;
+    $scope.starStats = {};
 
     RestData.getRestaurants()
     .then(function(data) {
-        main.restaurants = data;
-        $scope.$apply();
+        allData = data;
+
+        var starStats = {};
+        data.forEach(function(restaurant) { //gather stats
+            var avg = restaurant.average;
+            if(typeof starStats[avg] === 'undefined'){
+                starStats[avg] = 0;
+            }
+            starStats[avg]++;
+        });
+        $scope.starStats = starStats;
+
+        $scope.setFilter(0);
     });
+
+    $scope.openFilterMenu = function($mdOpenMenu, ev) {
+        originatorEv = ev;
+        $mdOpenMenu(ev);
+    };
+
+    $scope.setFilter = function(stars){
+        stars = parseInt(stars);
+        var data;
+        if(!stars){
+            data = allData;
+            $mdToast.showSimple('Showing all restaurants');
+        }else{
+            data = allData.filter(function(restaurant) {
+                return restaurant.average === stars;
+            });
+
+            $mdToast.showSimple('Showing '+stars+'-stars restaurants');
+        }
+        main.restaurants = data;
+    };
 
 }]);
