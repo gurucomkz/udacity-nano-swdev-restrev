b06f43b82080b0412269d136d4fad8c4c9ad7212
Sergey Shevchenko <sergey.aegis@gmail.com>
2016-09-21 13:31:04 +0700

    fix: include chromevox files into dist

--- Gruntfile.js
+++ Gruntfile.js
@@ -323,7 +323,8 @@ module.exports = function (grunt) {
             '*.{ico,png,txt}',
             '*.html',
             'images/{,*/}*.{webp}',
-            'styles/fonts/{,*/}*.*'
+            'styles/fonts/{,*/}*.*',
+            'chromevox/*.*'
           ]
         }, {
           expand: true,
