95078305c709daa70404d0916fa6b30139d80611
Sergey Shevchenko <sergey.aegis@gmail.com>
2016-09-21 12:45:38 +0700

    feat: make write review functionality better

--- app/scripts/controllers/writereview.js
+++ app/scripts/controllers/writereview.js
@@ -11,9 +11,31 @@ angular.module('restRevApp')
 .controller('WritereviewCtrl', [
     '$scope',
     '$routeParams',
-function ($scope, $routeParams) {
+    '$timeout',
+    '$mdToast',
+function ($scope, $routeParams, $timeout, $mdToast) {
+    $scope.sending = false;
+    $scope.author = {
+        email: '',
+        name: ''
+    };
+    $scope.review = {
+        stars: 3,
+        text: ''
+    };
     $scope.restaurant = {
         id: $routeParams.restaurantId,
         name: $routeParams.restaurantName
+    };
+
+    $scope.saveReview = function(){
+        $scope.sending = true;
+        $mdToast.showSimple('Review is being sent...');
+
+        $timeout(function() {
+            $mdToast.showSimple('Review sent. Thank you.');
+            $scope.sending = false;
+            window.location = '#/';
+        }, 3000);
     };
 }]);
